c6d9a72017e5771fbc986b5373a8cd95ef715f21
luânps34917 <130386979+luanhuupuannn@users.noreply.github.com>
2024-06-10 11:00:20 +0700

    sửa nhân vật

--- Assets/Luan/codebansung.cs
+++ Assets/Luan/codebansung.cs
@@ -6,13 +6,10 @@ using TMPro;
 
 public class codebansung : MonoBehaviour
 {
-    public Transform tâm;
-    public Transform súng; // Transform where the bullet spawns
-    public Transform gocnhin; // Transform where the bullet spawns
+    public Transform addfire;
     public TextMeshPro loadsodan;
 
 
-    public GameObject ak47;
 
     public int sodan;
 
@@ -26,7 +23,6 @@ public class codebansung : MonoBehaviour
     float fireRate = 0.1f; // Time between shots in seconds (adjust for desired rate)
 
     private float nextFireTime = 0f; // Time when the next bullet can be fired
-
     //private Animator animator;
 
     private void Start()
@@ -49,15 +45,12 @@ public class codebansung : MonoBehaviour
 
           //  animator.SetBool("bo", true);
             Invoke("huynapdan", 2f);
-
         }
         // Check if left mouse button is held down
         if (Input.GetMouseButton(0) && sodan > 0 && Time.time >= nextFireTime)
         {
           //  animator.SetBool("banmark", true);
-
             // Fire a bullet
-
             FireBullet();
             // Update next fire time based on fire rate
             nextFireTime = Time.time + fireRate;
@@ -81,65 +74,25 @@ public class codebansung : MonoBehaviour
     }
 
 
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-
-
-
-
-
-
-        if (other.gameObject.tag == "ak")
-        {
-
-
-            ak47.SetActive(true);
-            Destroy(other.gameObject);
-
-
-
-
-
-
-
-
-        }
-        else
-        {
-            fag = false;
-
-        }
-    }
     void huynapdan()
     {
       //  animator.SetBool("bo", false);
         sodan = 30;
         loadsodan.text = "" + sodan;
-
-
     }
     private void FireBullet()
     {
         // Instantiate a bullet from the prefab
-        GameObject bullet = Instantiate(bulletPrefab, súng.position, súng.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, addfire.position, addfire.rotation);
 
         // Apply forward force to the bullet
-        bullet.GetComponent<Rigidbody>().AddForce(súng.forward * bulletSpeed * forceMultiplier, ForceMode.Impulse);
+        bullet.GetComponent<Rigidbody>().AddForce(addfire.forward * bulletSpeed * forceMultiplier, ForceMode.Impulse);
         sodan -= 1;
 
         loadsodan.text = "" + sodan;
     }
 
-   // private void AimGunAtTarget()
-   // {
-        //  Calculate the direction from the gun to the aim point
-   //     Vector3 direction = tâm.position - gocnhin.position;
-
-        // Rotate the gun to face the direction (ignoring up/down rotation)
-   //     gocnhin.rotation = Quaternion.LookRotation(direction, Vector3.forward);
-   // }
+   
 
 
 }
