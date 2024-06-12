f259bfecb9c05b3613d45118cd6cb1e8f24e740d
luânps34917 <130386979+luanhuupuannn@users.noreply.github.com>
2024-06-13 00:01:11 +0700

    thanh máu nhân vật

---
+++ Assets/Luan/mauplyer1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mauplyer1 : MonoBehaviour
+{
+
+    public thanhmau thanhmau;
+    public float nowhp;
+    public float maxhp = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // cập nhật thanh máu 
+        nowhp = maxhp;
+        thanhmau.capnhatthanhmau(nowhp, maxhp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "player2") {
+
+            nowhp -= 2;
+            thanhmau.capnhatthanhmau(nowhp, maxhp);
+
+        }
+    }
+}
